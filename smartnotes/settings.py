b18396bfa4af9dcb556f454be1a7c02036645c8d
Marcos Mar <marcosabel0810@gmail.com>
2023-08-22 10:30:00 -0600

    Cambio variables de entorno

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -87,8 +87,7 @@ WSGI_APPLICATION = 'smartnotes.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
 }
